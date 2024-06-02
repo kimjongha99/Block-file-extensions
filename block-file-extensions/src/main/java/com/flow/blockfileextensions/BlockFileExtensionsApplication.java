2e763147c67a2c88f131e1e13ecce81f3c726e40
kimjongha99 <jjhsun99@gmail.com>
2024-06-02 22:54:32 +0900

    add: 깃이그노어 설정

---
+++ block-file-extensions/src/main/java/com/flow/blockfileextensions/BlockFileExtensionsApplication.java
@@ -0,0 +1,13 @@
+package com.flow.blockfileextensions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockFileExtensionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockFileExtensionsApplication.class, args);
+    }
+
+}
---
+++ block-file-extensions/src/test/java/com/flow/blockfileextensions/BlockFileExtensionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.blockfileextensions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockFileExtensionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
